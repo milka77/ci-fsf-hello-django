3b815e33f588acf6b98a244cea571ad78db131b8
milka77 <55999046+milka77@users.noreply.github.com>
2020-07-25 19:30:50 +0100

    added Procfile

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djnago_todo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_todo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
